e1230fe801d0cc481fd0a4f301fb7f0f2ce8c144
RA3D <boulaich.zaka@gmail.com>
2014-12-21 18:33:09 +0100

    Update Observers.java

--- src/main/java/fr/unice/vicc/Observers.java
+++ src/main/java/fr/unice/vicc/Observers.java
@@ -1,5 +1,9 @@
 package fr.unice.vicc;
 
+import java.util.List;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.power.PowerHost;
+
 /**
  * Just a container to declare your home-made observers.
  *
@@ -7,11 +11,15 @@ package fr.unice.vicc;
  * @author Fabien Hermenier
  */
 public class Observers {
+    public static  AntiAffinityObserver antiAO;// anti affinity observer
 
     /**
      * Build all the observers.
      */
-    public void build() {
+    public void build(List<PowerHost> hosts, List<Vm> vms) {
+        
+        antiAO = new AntiAffinityObserver(hosts);
+        antiAO.startEntity();
 
     }
 }
